aef1d05f0e21b6536b61b8f64acd3ccbd43cfc84
Paweł Kłodowski <angryface97@gmail.com>
2022-05-16 22:28:33 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const range = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+range.addEventListener("input", () => {
+  const rangeValue = range.value;
+  text.style.fontSize = rangeValue + "px";
+});
