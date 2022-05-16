446df60ddb343d31e33f073748fb46263485a843
Paweł Kłodowski <angryface97@gmail.com>
2022-05-16 22:20:30 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const img = document.querySelector(".gallery");
+
+const markup = images
+  .map(
+    (image) =>
+      `<li class="gallery_img"><img src="${image.url}" alt="${image.alt}"></li>`
+  )
+  .join("");
+
+img.insertAdjacentHTML("beforeend", markup);
