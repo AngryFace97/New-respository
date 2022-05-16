9fae3c6855f1886f41e9205f7f7f6ac92ffb7214
Paweł Kłodowski <angryface97@gmail.com>
2022-05-16 22:12:55 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
-const cl = (x) => console.log(x);
 // Task 1
 const numberOfCategories = Array.from(
   document.querySelectorAll("#categories > li")
 );
-cl("Number of categories: " + numberOfCategories.length);
+console.log("Number of categories: " + numberOfCategories.length);
 const item = document.querySelector(".item");
 const category = document.querySelectorAll(".item > h2");
 const elements = document.querySelectorAll(".item > ul");
@@ -11,5 +10,5 @@ for (let i = 0; i < category.length; i++) {
   let title = category[i].textContent;
   let listArr = elements[i].querySelectorAll("li");
   let listLen = listArr.length;
-  cl("Category: " + title + "\n" + "Elements: " + listLen);
+  console.log("Category: " + title + "\n" + "Elements: " + listLen);
 }
